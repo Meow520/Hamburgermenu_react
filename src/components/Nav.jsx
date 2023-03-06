e21dcf81435f7c8865f98b65f53c4d5fd4c6040d
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2023-03-06 15:21:58 +0900

    fix css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-export const Nav = ({ isOpen }) => {
-  const [name, setName] = useState("closeNav");
-  useEffect(() => {
-    if (name === "openNav") {
-      setName("closeNav");
-    } else {
-      setName("openNav");
-    }
-    console.log(name);
-  }, [isOpen]);
+export const Nav = ({isOpen }) => {
 
   return (
-    <div className={name}>
+    <div className="nav" hidden={!isOpen}>
       <div className="navText">Home</div>
       <div className="navText">About</div>
       <div className="navText">Work</div>
